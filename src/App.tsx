73c9ca7c9475de53d1899e5308239208d46830d8
navish davedi <navidavedi@gmail.com>
2020-05-16 19:13:12 +0530

    TESTING CICD

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Navish!!
+          Hello Navish, Testsing CICD!!
         </p>
       </header>
     </div>
