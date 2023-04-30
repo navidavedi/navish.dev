bd9c3748bf064d0177690595b15ac46671395c03
navish davedi <navidavedi@gmail.com>
2023-04-30 09:58:19 +0530

    feat: added gtag support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
 import { theme } from "../../theme";
+import Script from "next/script";
 
 export default function Document() {
 	return (
@@ -10,6 +11,19 @@ export default function Document() {
 				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 				<Main />
 				<NextScript />
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-98ZGL6CXK4"
+					strategy="afterInteractive"
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{` window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', 'G-98ZGL6CXK4');
+					`}
+				</Script>
 			</body>
 		</Html>
 	);
